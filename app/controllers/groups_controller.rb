59e024e88233757c4b413b45b4148765732e7ccb
西影行 <richardchen0954@gmail.com>
2018-12-18 09:15:07 +0800

    bootstrap

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,59 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts.order("id DESC")
+  end
+
+  def edit
+  end
+
+  def update
+
+  if @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+  else
+    render :edit
+  end
+  end
+
+    def destroy
+      @group.destroy
+      flash[:alert] = "Group deleted"
+      redirect_to groups_path
+    end
+
+  def new
+    @group = Group.new
+  end
+
+   def create
+     @group = Group.new(group_params)
+     @group.user = current_user
+
+     if @group.save
+       redirect_to groups_path
+     else
+       render :new
+     end
+   end
+
+   private
+
+   def group_params
+     params.require(:group).permit(:title, :description)
+   end
+
+   def find_group_and_check_permission
+     @group = Group.find(params[:id])
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+     end
+   end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+
+ before_action :authenticate_user!, :only => [:new, :create, :destroy]
+
+ def new
+   @group = Group.find(params[:group_id])
+   @post = Post.new
+ end
+
+ def create
+   @group = Group.find(params[:group_id])
+   @post = Post.new(post_params)
+   @post.group = @group
+   @post.user = current_user
+
+   if @post.save
+     redirect_to group_path(@group)
+   else
+     render :new
+   end
+ end
+
+    def destroy
+      @group = Group.find(params[:id])
+      @post = current_user.posts.find(params[:group_id]) # 只能删除自己的贴文
+      @post.destroy
+
+    end
+
+ private
+
+ def post_params
+   params.require(:post).permit(:content)
+ end
+
+end
